5550e5b2e83537b4b66b2ce08de7b6084cd2022c
Anna Garcia <annagarcia@Annas-MacBook-Pro.local>
2015-12-14 21:28:08 -0500

    use angular to show testimonials

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 var app = angular.module("myApp", ['ui.bootstrap']);
 
 app.controller("AppController", ['$scope', '$modal', '$log', function($scope, $modal, $log){
+	$scope.testimonials =[
+	{'name':'Jake G.', 'quote':'Had the time of my life...Wow I have never had so many varieties of juice and SO MUCH OF IT!', 'image':'jake.png'},
+	{'name':'Shameka K.', 'quote':'Everyone should try it at least once. I discovered great NYC juice bars that I didn\'t even know existed, and I made some new friends.','image':'shameka.png'},
+	{'name':'Anne M.', 'quote':'What a blast! I absolutely LOVE these events and look forward to going to more. I get to meet new people, connect with like minded health lovers, and I feel amazing drinking juice all day.', 'image':'anne.png'}
+
+	];
+
+    $scope.init = function(){
+		console.log('APP CONTROLLER: INIT');
+	};
 
 	$scope.status = {
 		isopen: false
